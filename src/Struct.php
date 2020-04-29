e9631251af3c17d1513c479db1e8aaf3d51ae8ec
Joshua Behrens <behrens@heptacom.de>
2020-04-29 02:53:49 +0200

    Add struct base

---
+++ src/Struct.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Dataset\Base;
+
+abstract class Struct implements \JsonSerializable
+{
+    public function jsonSerialize(): array
+    {
+        $vars = \get_object_vars($this);
+
+        /**
+         * @var string|int              $property
+         * @var string|int|float|object $value
+         */
+        foreach ($vars as $property => $value) {
+            if ($value instanceof \DateTimeInterface) {
+                $value = $value->format(\DateTimeInterface::ATOM);
+            }
+
+            $vars[$property] = $value;
+        }
+
+        return $vars;
+    }
+}
