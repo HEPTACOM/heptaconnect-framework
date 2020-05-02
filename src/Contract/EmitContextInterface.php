f04e74b446c83a11579b731162b76114cab39a01
Joshua Behrens <behrens@heptacom.de>
2020-05-03 01:02:11 +0200

    Remove emit method
    SPBCC-11

--- src/Contract/EmitContextInterface.php
+++ src/Contract/EmitContextInterface.php
@@ -2,12 +2,9 @@
 
 namespace Heptacom\HeptaConnect\Portal\Base\Contract;
 
-use Heptacom\HeptaConnect\Dataset\Base\Contract\DatasetEntityInterface;
 use Symfony\Component\OptionsResolver\Options;
 
 interface EmitContextInterface extends PortalNodeAwareInterface
 {
     public function getConfig(MappingInterface $mapping): Options;
-
-    public function emit(DatasetEntityInterface $datasetEntity, MappingInterface $mapping): void;
 }
