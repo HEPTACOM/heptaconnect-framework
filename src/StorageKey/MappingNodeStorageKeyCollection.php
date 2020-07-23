be8f09335669d96518ac6764db6c48c7dd9b96e8
Joshua Behrens <behrens@heptacom.de>
2020-07-23 18:14:46 +0200

    Add key storage collections
    SPBCC-11

---
+++ src/StorageKey/MappingNodeStorageKeyCollection.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Portal\Base\StorageKey;
+
+use Heptacom\HeptaConnect\Dataset\Base\Support\AbstractCollection;
+use Heptacom\HeptaConnect\Portal\Base\Contract\MappingNodeKeyInterface;
+
+/**
+ * @extends \Heptacom\HeptaConnect\Dataset\Base\Support\AbstractCollection<\Heptacom\HeptaConnect\Portal\Base\Contract\MappingNodeKeyInterface>
+ */
+class MappingNodeStorageKeyCollection extends AbstractCollection
+{
+    protected function isValidItem($item): bool
+    {
+        /* @phpstan-ignore-next-line treatPhpDocTypesAsCertain checks soft check but this is the hard check */
+        return $item instanceof MappingNodeKeyInterface;
+    }
+}
---
+++ test/StorageKeyCollectionTest.php
@@ -0,0 +1,71 @@
+<?php declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Portal\Base\Test;
+
+use Heptacom\HeptaConnect\Portal\Base\Contract\MappingNodeKeyInterface;
+use Heptacom\HeptaConnect\Portal\Base\Contract\PortalNodeKeyInterface;
+use Heptacom\HeptaConnect\Portal\Base\Contract\StorageKeyInterface;
+use Heptacom\HeptaConnect\Portal\Base\Contract\WebhookKeyInterface;
+use Heptacom\HeptaConnect\Portal\Base\StorageKey\MappingNodeStorageKeyCollection;
+use Heptacom\HeptaConnect\Portal\Base\StorageKey\PortalNodeStorageKeyCollection;
+use Heptacom\HeptaConnect\Portal\Base\StorageKey\WebhookStorageKeyCollection;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Heptacom\HeptaConnect\Portal\Base\StorageKey\MappingNodeStorageKeyCollection
+ * @covers \Heptacom\HeptaConnect\Portal\Base\StorageKey\PortalNodeStorageKeyCollection
+ * @covers \Heptacom\HeptaConnect\Portal\Base\StorageKey\WebhookStorageKeyCollection
+ */
+class StorageKeyCollectionTest extends TestCase
+{
+    public function testMappingNodeStorageKeyCollection(): void
+    {
+        $collection = new MappingNodeStorageKeyCollection();
+        $collection->push([new class() implements MappingNodeKeyInterface {
+            public function equals(StorageKeyInterface $other): bool
+            {
+                return \get_class($other) === \get_class($this);
+            }
+
+            public function jsonSerialize()
+            {
+                return [];
+            }
+        }]);
+        static::assertCount(1, $collection);
+    }
+
+    public function testPortalNodeStorageKeyCollection(): void
+    {
+        $collection = new PortalNodeStorageKeyCollection();
+        $collection->push([new class() implements PortalNodeKeyInterface {
+            public function equals(StorageKeyInterface $other): bool
+            {
+                return \get_class($other) === \get_class($this);
+            }
+
+            public function jsonSerialize()
+            {
+                return [];
+            }
+        }]);
+        static::assertCount(1, $collection);
+    }
+
+    public function testWebhookStorageKeyCollection(): void
+    {
+        $collection = new WebhookStorageKeyCollection();
+        $collection->push([new class() implements WebhookKeyInterface {
+            public function equals(StorageKeyInterface $other): bool
+            {
+                return \get_class($other) === \get_class($this);
+            }
+
+            public function jsonSerialize()
+            {
+                return [];
+            }
+        }]);
+        static::assertCount(1, $collection);
+    }
+}
