3ff46282999dcd2923465b7aee4a75cc6d15c24f
Joshua Behrens <behrens@heptacom.de>
2020-07-26 20:59:50 +0200

    Move WebhookServiceInterface into Webhook namespace SPBCC-11

--- src/Webhook/Contract/WebhookServiceInterface.php
+++ src/Webhook/Contract/WebhookServiceInterface.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Heptacom\HeptaConnect\Portal\Base\Contract;
+namespace Heptacom\HeptaConnect\Portal\Base\Webhook\Contract;
 
 interface WebhookServiceInterface
 {
