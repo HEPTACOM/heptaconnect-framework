7389ae0310edbfc4e0bd5964c818ff0d2b270e3d
Julian Krzefski <krzefski@heptacom.de>
2020-07-27 00:00:38 +0200

    SPBCC-11: Provide service discovery for webhook service

---
+++ src/Support/ServiceDiscovery.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Portal\Base\Support;
+
+use Heptacom\HeptaConnect\Portal\Base\Webhook\Contract\WebhookServiceInterface;
+use Http\Discovery\ClassDiscovery;
+use Http\Discovery\Exception\ClassInstantiationFailedException;
+
+class ServiceDiscovery extends ClassDiscovery
+{
+    public static function findWebhookService(): WebhookServiceInterface
+    {
+        $service = static::instantiateClass(static::findOneByType(WebhookServiceInterface::class));
+
+        if (!$service instanceof WebhookServiceInterface) {
+            throw new ClassInstantiationFailedException();
+        }
+
+        return $service;
+    }
+}
