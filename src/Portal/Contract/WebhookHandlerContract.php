dd52e291b29ff3123666d5f7b7900e6418cd8e62
Julian Krzefski <krzefski@heptacom.de>
2020-07-26 21:12:55 +0200

    SPBCC-11: Move composer psr requirements and add WebhookHandlerContract

---
+++ src/Portal/Contract/WebhookHandlerContract.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Portal\Base\Portal\Contract;
+
+use Heptacom\HeptaConnect\Portal\Base\Contract\WebhookInterface;
+use Http\Discovery\Psr17FactoryDiscovery;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+
+abstract class WebhookHandlerContract
+{
+    protected ResponseFactoryInterface $responseFactory;
+
+    protected StreamFactoryInterface $streamFactory;
+
+    public function __construct()
+    {
+        $this->responseFactory = Psr17FactoryDiscovery::findResponseFactory();
+        $this->streamFactory = Psr17FactoryDiscovery::findStreamFactory();
+    }
+
+    public function handle(RequestInterface $request, WebhookInterface $webhook): ResponseInterface
+    {
+        return $this->responseFactory->createResponse(404);
+    }
+}
