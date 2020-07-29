d711303043d2337b562673e6106eb26e6dad628f
Joshua Behrens <behrens@heptacom.de>
2020-07-29 16:27:31 +0200

    Add service discovery test
    SPBCC-11

---
+++ test/Fixture/NonWebhookServiceStrategy.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Portal\Base\Test\Fixture;
+
+use Heptacom\HeptaConnect\Portal\Base\Webhook\Contract\WebhookServiceInterface;
+use Http\Discovery\Strategy\DiscoveryStrategy;
+
+class NonWebhookServiceStrategy implements DiscoveryStrategy
+{
+    public static function getCandidates($type)
+    {
+        return [
+            [
+                'condition' => fn () => \is_a($type, WebhookServiceInterface::class, true),
+                'class' => fn () => null,
+            ],
+        ];
+    }
+}
---
+++ test/Fixture/WebhookServiceStrategy.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Portal\Base\Test\Fixture;
+
+use Heptacom\HeptaConnect\Portal\Base\Webhook\Contract\WebhookInterface;
+use Heptacom\HeptaConnect\Portal\Base\Webhook\Contract\WebhookServiceInterface;
+use Http\Discovery\Strategy\DiscoveryStrategy;
+
+class WebhookServiceStrategy implements DiscoveryStrategy
+{
+    public static function getCandidates($type)
+    {
+        return [
+            [
+                'condition' => fn () => \is_a($type, WebhookServiceInterface::class, true),
+                'class' => fn () => new class implements WebhookServiceInterface {
+                    public function register(string $webhookHandler, ?array $payload = null): WebhookInterface
+                    {
+                        throw new \RuntimeException();
+                    }
+
+                    public function scheduleRefresh(WebhookInterface $webhook, \DateTimeInterface $dateTime): void
+                    {
+                    }
+                },
+            ],
+        ];
+    }
+}
