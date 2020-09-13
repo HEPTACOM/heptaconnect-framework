4a81928e13c41a67e57b11cb678cce4079f3d122
Joshua Behrens <behrens@heptacom.de>
2020-09-13 19:21:42 +0200

    Add serialization and deserialization method for keys and use it for external key storing CONNECT-22

--- src/Cronjob/Contract/CronjobServiceInterface.php
+++ src/Cronjob/Contract/CronjobServiceInterface.php
@@ -4,6 +4,7 @@ namespace Heptacom\HeptaConnect\Portal\Base\Cronjob\Contract;
 
 use Heptacom\HeptaConnect\Portal\Base\Cronjob\Exception\InvalidCronExpressionException;
 use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\CronjobKeyInterface;
+use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
 
 interface CronjobServiceInterface
 {
@@ -12,7 +13,12 @@ interface CronjobServiceInterface
      *
      * @throws InvalidCronExpressionException
      */
-    public function register(string $cronjobHandler, string $cronExpression, ?array $payload = null): CronjobInterface;
+    public function register(
+        PortalNodeKeyInterface $portalNodeKey,
+        string $cronjobHandler,
+        string $cronExpression,
+        ?array $payload = null
+    ): CronjobInterface;
 
     public function unregister(CronjobKeyInterface $cronjobKey): void;
 }
