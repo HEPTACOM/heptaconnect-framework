87bc66c86b7edb7faf11f3c65f02d5dbb2e97d65
Joshua Behrens <behrens@heptacom.de>
2020-09-18 23:15:56 +0200

    Extract RouteRepositoryContract from Storage CONNECT-22

---
+++ src/Portal/Contract/RouteInterface.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Portal\Base\Portal\Contract;
+
+use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
+use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\RouteKeyInterface;
+
+interface RouteInterface
+{
+    public function getKey(): RouteKeyInterface;
+
+    public function getTargetKey(): PortalNodeKeyInterface;
+
+    public function getSourceKey(): PortalNodeKeyInterface;
+
+    /**
+     * @psalm-return class-string<\Heptacom\HeptaConnect\Dataset\Base\Contract\DatasetEntityInterface>
+     */
+    public function getEntityClassName(): string;
+}
