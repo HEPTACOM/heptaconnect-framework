2b3e712b6410586012f13c4af03724c2bfe28f30
Julian Krzefski <krzefski@heptacom.de>
2020-10-25 18:28:15 +0100

    Exclude ReflectionMapping objects from DatasetEntityTracker

--- src/Support/DatasetEntityTracker.php
+++ src/Support/DatasetEntityTracker.php
@@ -6,12 +6,20 @@ use Heptacom\HeptaConnect\Dataset\Base\DatasetEntity;
 
 abstract class DatasetEntityTracker
 {
+    private static array $deniedClasses = [];
+
     private static array $contextStack = [];
 
     public static function report(DatasetEntity $entity): void
     {
         if (empty(self::$contextStack)) {
             return;
+        }
+
+        foreach (self::$deniedClasses as $deniedClass => $_) {
+            if ($entity instanceof $deniedClass) {
+                return;
+            }
         }
 
         $key = \array_key_last(self::$contextStack);
@@ -26,5 +34,15 @@ abstract class DatasetEntityTracker
     public static function retrieve(): TrackedEntityCollection
     {
         return new TrackedEntityCollection(\array_pop(self::$contextStack) ?? []);
+    }
+
+    public static function allow(string $className): void
+    {
+        unset(self::$deniedClasses[$className]);
+    }
+
+    public static function deny(string $className): void
+    {
+        self::$deniedClasses[$className] = true;
     }
 }
