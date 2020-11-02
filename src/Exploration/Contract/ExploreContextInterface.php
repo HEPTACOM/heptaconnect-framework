b033a0a3a27cd1820e7fd8b573ac9d103f595e32
Jana Schwalm <schwalm@heptacom.de>
2020-11-02 13:34:05 +0100

    add getPortalNodeKey

--- src/Exploration/Contract/ExploreContextInterface.php
+++ src/Exploration/Contract/ExploreContextInterface.php
@@ -5,9 +5,12 @@ namespace Heptacom\HeptaConnect\Portal\Base\Exploration\Contract;
 use Heptacom\HeptaConnect\Portal\Base\Parallelization\Support\ResourceLockFacade;
 use Heptacom\HeptaConnect\Portal\Base\Portal\Contract\PortalContract;
 use Heptacom\HeptaConnect\Portal\Base\Portal\Contract\PortalStorageInterface;
+use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
 
 interface ExploreContextInterface
 {
+    public function getPortalNodeKey(): PortalNodeKeyInterface;
+
     public function getPortal(): PortalContract;
 
     public function getConfig(): ?array;
--- src/Reception/Contract/ReceiveContextInterface.php
+++ src/Reception/Contract/ReceiveContextInterface.php
@@ -6,9 +6,12 @@ use Heptacom\HeptaConnect\Portal\Base\Mapping\Contract\MappingInterface;
 use Heptacom\HeptaConnect\Portal\Base\Parallelization\Support\ResourceLockFacade;
 use Heptacom\HeptaConnect\Portal\Base\Portal\Contract\PortalAwareInterface;
 use Heptacom\HeptaConnect\Portal\Base\Portal\Contract\PortalStorageInterface;
+use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
 
 interface ReceiveContextInterface extends PortalAwareInterface
 {
+    public function getPortalNodeKey(MappingInterface $mapping): PortalNodeKeyInterface;
+
     public function getConfig(MappingInterface $mapping): ?array;
 
     public function markAsFailed(MappingInterface $mapping, \Throwable $throwable): void;
