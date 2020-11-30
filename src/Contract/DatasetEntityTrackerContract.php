e859d8eb70218513fa8b2ed57533e4ed667dc7ab
Joshua Behrens <behrens@heptacom.de>
2020-11-30 01:32:20 +0100

    Extract dataset entity tracker into a contract

---
+++ src/Contract/DatasetEntityTrackerContract.php
@@ -0,0 +1,67 @@
+<?php declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Dataset\Base\Contract;
+
+use Heptacom\HeptaConnect\Dataset\Base\DatasetEntity;
+use Heptacom\HeptaConnect\Dataset\Base\Support\TrackedEntityCollection;
+
+class DatasetEntityTrackerContract
+{
+    /**
+     * @psalm-var array<class-string<\Heptacom\HeptaConnect\Dataset\Base\Contract\DatasetEntityInterface>, bool>
+     */
+    private array $deniedClasses = [];
+
+    /**
+     * @psalm-var array<array<\Heptacom\HeptaConnect\Dataset\Base\Contract\DatasetEntityInterface>>
+     */
+    private array $contextStack = [];
+
+    public function report(DatasetEntity $entity): void
+    {
+        if (\count($this->contextStack) === 0) {
+            return;
+        }
+
+        foreach ($this->deniedClasses as $deniedClass => $_) {
+            if ($entity instanceof $deniedClass) {
+                return;
+            }
+        }
+
+        /** @var string|int|null $key */
+        $key = \array_key_last($this->contextStack);
+
+        if (\is_null($key)) {
+            return;
+        }
+
+        $this->contextStack[$key][] = $entity;
+    }
+
+    public function listen(): void
+    {
+        $this->contextStack[] = [];
+    }
+
+    public function retrieve(): TrackedEntityCollection
+    {
+        return new TrackedEntityCollection(\array_pop($this->contextStack) ?? []);
+    }
+
+    /**
+     * @psalm-param class-string<\Heptacom\HeptaConnect\Dataset\Base\Contract\DatasetEntityInterface> $className
+     */
+    public function allow(string $className): void
+    {
+        unset($this->deniedClasses[$className]);
+    }
+
+    /**
+     * @psalm-param class-string<\Heptacom\HeptaConnect\Dataset\Base\Contract\DatasetEntityInterface> $className
+     */
+    public function deny(string $className): void
+    {
+        $this->deniedClasses[$className] = true;
+    }
+}
--- src/Support/DatasetEntityTracker.php
+++ src/Support/DatasetEntityTracker.php
@@ -2,74 +2,16 @@
 
 namespace Heptacom\HeptaConnect\Dataset\Base\Support;
 
-use Heptacom\HeptaConnect\Dataset\Base\DatasetEntity;
+use Heptacom\HeptaConnect\Dataset\Base\Contract\DatasetEntityTrackerContract;
 
-class DatasetEntityTracker
+class DatasetEntityTracker extends DatasetEntityTrackerContract
 {
-    /**
-     * @psalm-var array<class-string<\Heptacom\HeptaConnect\Dataset\Base\Contract\DatasetEntityInterface>, bool>
-     */
-    private array $deniedClasses = [];
-
-    /**
-     * @psalm-var array<array<\Heptacom\HeptaConnect\Dataset\Base\Contract\DatasetEntityInterface>>
-     */
-    private array $contextStack = [];
-
-    public function report(DatasetEntity $entity): void
-    {
-        if (\count($this->contextStack) === 0) {
-            return;
-        }
-
-        foreach ($this->deniedClasses as $deniedClass => $_) {
-            if ($entity instanceof $deniedClass) {
-                return;
-            }
-        }
-
-        /** @var string|int|null $key */
-        $key = \array_key_last($this->contextStack);
-
-        if (\is_null($key)) {
-            return;
-        }
-
-        $this->contextStack[$key][] = $entity;
-    }
-
-    public function listen(): void
-    {
-        $this->contextStack[] = [];
-    }
-
-    public function retrieve(): TrackedEntityCollection
-    {
-        return new TrackedEntityCollection(\array_pop($this->contextStack) ?? []);
-    }
-
-    /**
-     * @psalm-param class-string<\Heptacom\HeptaConnect\Dataset\Base\Contract\DatasetEntityInterface> $className
-     */
-    public function allow(string $className): void
-    {
-        unset($this->deniedClasses[$className]);
-    }
-
-    /**
-     * @psalm-param class-string<\Heptacom\HeptaConnect\Dataset\Base\Contract\DatasetEntityInterface> $className
-     */
-    public function deny(string $className): void
-    {
-        $this->deniedClasses[$className] = true;
-    }
-
-    public static function instance(): DatasetEntityTracker
+    public static function instance(): DatasetEntityTrackerContract
     {
         /** @var DatasetEntityTracker|null $instance */
         static $instance = null;
 
-        if (!$instance instanceof DatasetEntityTracker) {
+        if (!$instance instanceof DatasetEntityTrackerContract) {
             $instance = new DatasetEntityTracker();
         }
 
