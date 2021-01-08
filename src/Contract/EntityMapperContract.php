18a588b0887329cd90b7a234b15a4f32aec1b98c
Julian Krzefski <krzefski@heptacom.de>
2021-01-09 00:15:36 +0100

    Introduce EntityMapper component for increased performance

---
+++ src/Contract/EntityMapperContract.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Storage\Base\Contract;
+
+use Heptacom\HeptaConnect\Dataset\Base\DatasetEntityCollection;
+use Heptacom\HeptaConnect\Portal\Base\Mapping\MappedDatasetEntityCollection;
+use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
+
+abstract class EntityMapperContract
+{
+    abstract public function mapEntities(
+        DatasetEntityCollection $entityCollection,
+        PortalNodeKeyInterface $portalNodeKey
+    ): MappedDatasetEntityCollection;
+}
