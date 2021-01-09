9cdbf24de84274e670b6dca420be46f03ef0b6fe
Joshua Behrens <behrens@heptacom.de>
2021-01-09 23:07:55 +0100

    Generate multiple keys at once

--- src/Contract/StorageKeyGeneratorContract.php
+++ src/Contract/StorageKeyGeneratorContract.php
@@ -11,8 +11,19 @@ abstract class StorageKeyGeneratorContract
      * @psalm-param class-string<\Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\StorageKeyInterface> $keyClassName
      *
      * @throws UnsupportedStorageKeyException
+     *
+     * @deprecated Use generateKeys instead
      */
     abstract public function generateKey(string $keyClassName): StorageKeyInterface;
+
+    /**
+     * @psalm-param class-string<\Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\StorageKeyInterface> $keyClassName
+     *
+     * @throws UnsupportedStorageKeyException
+     * @return StorageKeyInterface[]
+     * @psalm-return iterable<int, \Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\StorageKeyInterface>
+     */
+    abstract public function generateKeys(string $keyClassName, int $count): iterable;
 
     /**
      * @throws UnsupportedStorageKeyException
