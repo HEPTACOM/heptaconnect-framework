5f48562aa3774117766c4aeb11e815c7b0724bcc
Joshua Behrens <behrens@heptacom.de>
2021-03-02 13:42:25 +0100

    Add entity status contract to check emission state by receivers

--- src/Reception/Contract/ReceiveContextInterface.php
+++ src/Reception/Contract/ReceiveContextInterface.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 namespace Heptacom\HeptaConnect\Portal\Base\Reception\Contract;
 
 use Heptacom\HeptaConnect\Portal\Base\Portal\Contract\StatelessContextInterface;
+use Heptacom\HeptaConnect\Portal\Base\Support\Contract\EntityStatusContract;
 
 interface ReceiveContextInterface extends StatelessContextInterface
 {
+    public function getEntityStatus(): EntityStatusContract;
 }
