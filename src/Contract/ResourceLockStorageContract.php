3465365ca0f474b12c9e38fcfdc956dbac26d7d4
Julian Krzefski <krzefski@heptacom.de>
2021-05-29 18:47:25 +0200

    Mark resource locking as internal

--- src/Contract/ResourceLockStorageContract.php
+++ src/Contract/ResourceLockStorageContract.php
@@ -3,6 +3,15 @@ declare(strict_types=1);
 
 namespace Heptacom\HeptaConnect\Storage\Base\Contract;
 
+/**
+ * Resource locking is currently not working properly
+ * as locks are released prematurely. This depends on
+ * the underlying storage implementation. Currently
+ * it is unclear whether a fix will require breaking
+ * changes to the public api.
+ *
+ * @internal
+ */
 abstract class ResourceLockStorageContract
 {
     abstract public function create(string $key): void;
