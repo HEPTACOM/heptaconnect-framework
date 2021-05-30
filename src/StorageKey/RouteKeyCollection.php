f0fc22c52582851d4acd393861cc520f6f2cc8bf
Joshua Behrens <behrens@heptacom.de>
2021-05-30 03:05:02 +0200

    Add route key collection
    CONNECT-154

---
+++ src/StorageKey/RouteKeyCollection.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Portal\Base\StorageKey;
+
+use Heptacom\HeptaConnect\Dataset\Base\Support\AbstractCollection;
+use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\RouteKeyInterface;
+
+/**
+ * @extends \Heptacom\HeptaConnect\Dataset\Base\Support\AbstractCollection<\Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\RouteKeyInterface>
+ */
+class RouteKeyCollection extends AbstractCollection
+{
+    protected function isValidItem($item): bool
+    {
+        /* @phpstan-ignore-next-line treatPhpDocTypesAsCertain checks soft check but this is the hard check */
+        return $item instanceof RouteKeyInterface;
+    }
+}
