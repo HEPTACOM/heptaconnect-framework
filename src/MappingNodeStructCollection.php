6291293513a0860eaf40422433a468ea0be7a5a3
Joshua Behrens <behrens@heptacom.de>
2021-06-05 19:51:09 +0200

    Use AbstractObjectCollection instead of DatasetEntityCollection

--- src/MappingNodeStructCollection.php
+++ src/MappingNodeStructCollection.php
@@ -3,13 +3,13 @@ declare(strict_types=1);
 
 namespace Heptacom\HeptaConnect\Storage\Base;
 
-use Heptacom\HeptaConnect\Dataset\Base\DatasetEntityCollection;
+use Heptacom\HeptaConnect\Dataset\Base\Support\AbstractObjectCollection;
 use Heptacom\HeptaConnect\Storage\Base\Contract\MappingNodeStructInterface;
 
 /**
- * @extends \Heptacom\HeptaConnect\Dataset\Base\DatasetEntityCollection<\Heptacom\HeptaConnect\Storage\Base\Contract\MappingNodeStructInterface>
+ * @extends \Heptacom\HeptaConnect\Dataset\Base\Support\AbstractObjectCollection<\Heptacom\HeptaConnect\Storage\Base\Contract\MappingNodeStructInterface>
  */
-class MappingNodeStructCollection extends DatasetEntityCollection
+class MappingNodeStructCollection extends AbstractObjectCollection
 {
     protected function getT(): string
     {
