099a004676a9a43888aa72af2948bca3bb7bbfd8
Julian Krzefski <krzefski@heptacom.de>
2021-06-10 16:50:23 +0200

    Add profiling

---
+++ src/Profiling/NullProfiler.php
@@ -0,0 +1,7 @@
+<?php declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Portal\Base\Profiling;
+
+class NullProfiler extends ProfilerContract
+{
+}
---
+++ src/Profiling/ProfilerFactoryContract.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Portal\Base\Profiling;
+
+abstract class ProfilerFactoryContract
+{
+    abstract public function factory(?string $prefix = null): ProfilerContract;
+}
