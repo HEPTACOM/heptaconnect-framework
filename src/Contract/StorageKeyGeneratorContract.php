b2e36ef85a7cffd1354f2cf2fd6ab83a15536564
Joshua Behrens <behrens@heptacom.de>
2021-06-13 17:20:48 +0200

    Add preview portal node keys
    CONNECT-92

--- src/Contract/StorageKeyGeneratorContract.php
+++ src/Contract/StorageKeyGeneratorContract.php
@@ -3,8 +3,10 @@ declare(strict_types=1);
 
 namespace Heptacom\HeptaConnect\Storage\Base\Contract;
 
+use Heptacom\HeptaConnect\Portal\Base\Portal\Contract\PortalContract;
 use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\StorageKeyInterface;
 use Heptacom\HeptaConnect\Storage\Base\Exception\UnsupportedStorageKeyException;
+use Heptacom\HeptaConnect\Storage\Base\PreviewPortalNodeKey;
 
 abstract class StorageKeyGeneratorContract
 {
@@ -30,10 +32,36 @@ abstract class StorageKeyGeneratorContract
     /**
      * @throws UnsupportedStorageKeyException
      */
-    abstract public function serialize(StorageKeyInterface $key): string;
+    public function serialize(StorageKeyInterface $key): string
+    {
+        if ($key instanceof PreviewPortalNodeKey) {
+            try {
+                return \json_encode($key, \JSON_THROW_ON_ERROR);
+            } catch (\Throwable $throwable) {
+                throw new UnsupportedStorageKeyException(\get_class($key), $throwable);
+            }
+        }
+
+        throw new UnsupportedStorageKeyException(\get_class($key));
+    }
 
     /**
      * @throws UnsupportedStorageKeyException
      */
-    abstract public function deserialize(string $keyData): StorageKeyInterface;
+    public function deserialize(string $keyData): StorageKeyInterface
+    {
+        try {
+            $json = (array) \json_decode($keyData, true, 3, \JSON_THROW_ON_ERROR);
+
+            $portalType = (string) ($json['preview'] ?? null);
+
+            if (\is_a($portalType, PortalContract::class, true)) {
+                return new PreviewPortalNodeKey($portalType);
+            }
+        } catch (\Throwable $throwable) {
+            throw new UnsupportedStorageKeyException(StorageKeyInterface::class, $throwable);
+        }
+
+        throw new UnsupportedStorageKeyException(StorageKeyInterface::class);
+    }
 }
