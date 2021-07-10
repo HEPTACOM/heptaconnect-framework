b860c52c2c722eeca3a8ea0e1c2d47f219957bae
Julian Krzefski <krzefski@heptacom.de>
2021-07-10 22:12:03 +0200

    Add contract for DirectEmissionFlow and add it as default utility

---
+++ src/Flow/DirectEmission/DirectEmissionFlowContract.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Portal\Base\Flow\DirectEmission;
+
+use Heptacom\HeptaConnect\Dataset\Base\DatasetEntityCollection;
+use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
+
+abstract class DirectEmissionFlowContract
+{
+    abstract public function run(PortalNodeKeyInterface $portalNodeKey, DatasetEntityCollection $entities): DirectEmissionResult;
+}
---
+++ src/Flow/DirectEmission/DirectEmissionResult.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Portal\Base\Flow\DirectEmission;
+
+class DirectEmissionResult
+{
+    private array $errors = [];
+
+    public function addError(\Throwable $error): void
+    {
+        $this->errors[] = $error;
+    }
+
+    public function hasErrors(): bool
+    {
+        return (bool) $this->errors;
+    }
+
+    /**
+     * @return \Throwable[]
+     */
+    public function getErrors(): array
+    {
+        return $this->errors;
+    }
+}
