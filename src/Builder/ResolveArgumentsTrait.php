14bae913fa5e4e93037aeadb69c07a9e08c6d920
Julian Krzefski <krzefski@heptacom.de>
2021-07-11 17:55:05 +0200

    Support binding config keys in short notation for flow components

--- src/Builder/ResolveArgumentsTrait.php
+++ src/Builder/ResolveArgumentsTrait.php
@@ -3,16 +3,21 @@ declare(strict_types=1);
 
 namespace Heptacom\HeptaConnect\Portal\Base\Builder;
 
+use Heptacom\HeptaConnect\Portal\Base\Portal\Contract\ConfigurationContract;
 use Heptacom\HeptaConnect\Portal\Base\Portal\Contract\PortalNodeContextInterface;
+use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
 use Psr\Container\ContainerInterface;
 
 trait ResolveArgumentsTrait
 {
+    private array $bindingKeyCache = [];
+
     protected function resolveArguments(
         callable $method,
         PortalNodeContextInterface $context,
         callable $resolveArgument
     ): array {
+        /** @var ContainerInterface $container */
         $container = $context->getContainer();
 
         if (\is_array($method)) {
@@ -23,17 +28,21 @@ trait ResolveArgumentsTrait
             $reflection = new \ReflectionFunction($method);
         }
 
+        $bindingKeys = $this->getBindingKeys($container);
         $arguments = [];
 
         foreach ($reflection->getParameters() as $key => $param) {
             $type = $this->getType($param, $reflection);
+            $parameterName = $param->getName();
 
             if (\is_a($type, PortalNodeContextInterface::class, true)) {
                 $arguments[] = $context;
+            } elseif ($this->isParameterScalarish($param) && isset($bindingKeys[$parameterName])) {
+                $arguments[] = $bindingKeys[$parameterName];
             } else {
                 $arguments[] = $resolveArgument(
                     (int) $key,
-                    $param->getName(),
+                    $parameterName,
                     $type,
                     $container
                 );
@@ -71,5 +80,80 @@ trait ResolveArgumentsTrait
         }
 
         return $container->get($propertyType);
+    }
+
+    private function getBindingKeys(ContainerInterface $container): array
+    {
+        /** @var PortalNodeKeyInterface $currentPortalNodeKey */
+        $currentPortalNodeKey = $container->get(PortalNodeKeyInterface::class);
+
+        foreach ($this->bindingKeyCache as $cacheItem) {
+            $comparePortalNodeKey = $cacheItem['portalNodeKey'] ?? null;
+
+            if (!$comparePortalNodeKey instanceof PortalNodeKeyInterface) {
+                continue;
+            }
+
+            if (!$currentPortalNodeKey->equals($comparePortalNodeKey)) {
+                continue;
+            }
+
+            $data = $cacheItem['data'] ?? null;
+
+            if (\is_array($data)) {
+                return $data;
+            }
+        }
+
+        /** @var ConfigurationContract $config */
+        $config = $container->get(ConfigurationContract::class);
+
+        $configKeySeparators = '_.-';
+        $bindingKeys = [];
+
+        foreach ($config->keys() as $configurationName) {
+            $parameterName = \str_replace(
+                \str_split($configKeySeparators),
+                '',
+                \ucwords($configurationName, $configKeySeparators)
+            );
+
+            $bindingKeys['config'.$parameterName] = $config->get($configurationName);
+        }
+
+        $this->bindingKeyCache[] = [
+            'portalNodeKey' => $currentPortalNodeKey,
+            'data' => $bindingKeys,
+        ];
+
+        return $bindingKeys;
+    }
+
+    private function isParameterScalarish(\ReflectionParameter $parameter): bool
+    {
+        foreach ($this->getParameterTypes($parameter->getType()) as $type) {
+            if (\class_exists($type)) {
+                return false;
+            }
+
+            if (\in_array(\mb_strtolower($type), ['string', 'float', 'int', 'bool', 'array'], true)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private function getParameterTypes(?\ReflectionType $type): array
+    {
+        if ($type instanceof \ReflectionNamedType) {
+            return [$type->getName()];
+        }
+
+        if ($type instanceof \ReflectionUnionType) {
+            return \array_merge([], ...\array_map([$this, 'getParameterTypes'], $type->getTypes()));
+        }
+
+        return [];
     }
 }
