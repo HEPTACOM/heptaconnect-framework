cfe437fa1a4d1fb7d23c8d1b925e917c4255566e
Julian Krzefski <krzefski@heptacom.de>
2021-09-11 21:13:34 +0200

    CONNECT-174: Add more info to MappingConflictException

--- src/MappingPersister/Exception/MappingConflictException.php
+++ src/MappingPersister/Exception/MappingConflictException.php
@@ -8,6 +8,8 @@ use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface
 
 class MappingConflictException extends \Exception
 {
+    public const FORMAT = 'Conflicting mapping; PortalNode: %s; MappingNode: %s; PrimaryKey: %s';
+
     private PortalNodeKeyInterface $portalNodeKey;
 
     private MappingNodeKeyInterface $mappingNodeKey;
@@ -15,15 +17,29 @@ class MappingConflictException extends \Exception
     private string $externalId;
 
     public function __construct(
+        string $message,
         PortalNodeKeyInterface $portalNodeKey,
         MappingNodeKeyInterface $mappingNodeKey,
         string $externalId
     ) {
-        $message = 'CONFLICT '.$externalId;
-
         parent::__construct($message);
         $this->portalNodeKey = $portalNodeKey;
         $this->mappingNodeKey = $mappingNodeKey;
         $this->externalId = $externalId;
+    }
+
+    public function getPortalNodeKey(): PortalNodeKeyInterface
+    {
+        return $this->portalNodeKey;
+    }
+
+    public function getMappingNodeKey(): MappingNodeKeyInterface
+    {
+        return $this->mappingNodeKey;
+    }
+
+    public function getExternalId(): string
+    {
+        return $this->externalId;
     }
 }
