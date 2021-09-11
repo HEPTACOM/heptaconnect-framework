838e87d9825af2e719ed9e612ca9db70b44ca43c
Julian Krzefski <krzefski@heptacom.de>
2021-09-11 18:24:33 +0200

    CONNECT-174: Add validation step in MappingPersister to detect conflicts

---
+++ src/Exception/MappingConflictException.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Storage\Base\Exception;
+
+use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\MappingNodeKeyInterface;
+use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
+
+class MappingConflictException extends \Exception
+{
+    private PortalNodeKeyInterface $portalNodeKey;
+
+    private MappingNodeKeyInterface $mappingNodeKey;
+
+    private string $externalId;
+
+    public function __construct(
+        PortalNodeKeyInterface $portalNodeKey,
+        MappingNodeKeyInterface $mappingNodeKey,
+        string $externalId
+    ) {
+        $message = 'CONFLICT '.$externalId;
+
+        parent::__construct($message);
+        $this->portalNodeKey = $portalNodeKey;
+        $this->mappingNodeKey = $mappingNodeKey;
+        $this->externalId = $externalId;
+    }
+}
