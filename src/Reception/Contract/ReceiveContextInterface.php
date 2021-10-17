6a97a0784277403fd07282913c70fb9e8bde717f
Joshua Behrens <behrens@heptacom.de>
2021-10-17 16:01:27 +0200

    Change EventDispatcherInterface to PSR instead of symfony
    CONNECT-109

--- src/Reception/Contract/ReceiveContextInterface.php
+++ src/Reception/Contract/ReceiveContextInterface.php
@@ -6,7 +6,7 @@ namespace Heptacom\HeptaConnect\Portal\Base\Reception\Contract;
 use Heptacom\HeptaConnect\Core\Reception\Support\PostProcessorDataBag;
 use Heptacom\HeptaConnect\Portal\Base\Portal\Contract\PortalNodeContextInterface;
 use Heptacom\HeptaConnect\Portal\Base\Support\Contract\EntityStatusContract;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Psr\EventDispatcher\EventDispatcherInterface;
 
 interface ReceiveContextInterface extends PortalNodeContextInterface
 {
