f8a0c296e65877f98ffe1b04bef4792963812bd0
Joshua Behrens <behrens@heptacom.de>
2021-11-09 23:05:28 +0100

    Extract overview criteria components into base class
    CONNECT-288

--- src/Contract/RouteOverviewCriteria.php
+++ src/Contract/RouteOverviewCriteria.php
@@ -3,12 +3,10 @@ declare(strict_types=1);
 
 namespace Heptacom\HeptaConnect\Storage\Base\Contract;
 
-class RouteOverviewCriteria
+use Heptacom\HeptaConnect\Storage\Base\Contract\Action\Overview\OverviewCriteriaContract;
+
+class RouteOverviewCriteria extends OverviewCriteriaContract
 {
-    public const SORT_ASC = 'asc';
-
-    public const SORT_DESC = 'desc';
-
     public const FIELD_TARGET = 'target';
 
     public const FIELD_SOURCE = 'source';
@@ -17,41 +15,7 @@ class RouteOverviewCriteria
 
     public const FIELD_CREATED = 'created';
 
-    protected int $page = 0;
-
-    protected ?int $pageSize = null;
-
     protected array $sort = [
         self::FIELD_CREATED => self::SORT_ASC,
     ];
-
-    public function getPage(): int
-    {
-        return $this->page;
-    }
-
-    public function setPage(int $page): void
-    {
-        $this->page = $page;
-    }
-
-    public function getPageSize(): ?int
-    {
-        return $this->pageSize;
-    }
-
-    public function setPageSize(?int $pageSize): void
-    {
-        $this->pageSize = $pageSize;
-    }
-
-    public function getSort(): array
-    {
-        return $this->sort;
-    }
-
-    public function setSort(array $sort): void
-    {
-        $this->sort = $sort;
-    }
 }
