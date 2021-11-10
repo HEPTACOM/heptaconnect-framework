d4bdf15274c5723334a393a0d31847988841141a
Joshua Behrens <behrens@heptacom.de>
2021-11-10 01:31:31 +0100

    Add capabilities to RouteGet
    CONNECT-288

--- src/Contract/RouteGetResult.php
+++ src/Contract/RouteGetResult.php
@@ -15,7 +15,15 @@ class RouteGetResult implements RouteInterface
 
     protected PortalNodeKeyInterface $target;
 
+    /**
+     * @var class-string<\Heptacom\HeptaConnect\Dataset\Base\Contract\DatasetEntityContract>
+     */
     protected string $entityType;
+
+    /**
+     * @var string[]
+     */
+    protected array $capabilities;
 
     /**
      * @param class-string<\Heptacom\HeptaConnect\Dataset\Base\Contract\DatasetEntityContract> $entityType
@@ -24,12 +32,14 @@ class RouteGetResult implements RouteInterface
         RouteKeyInterface $route,
         PortalNodeKeyInterface $source,
         PortalNodeKeyInterface $target,
-        string $entityType
+        string $entityType,
+        array $capabilities
     ) {
         $this->route = $route;
         $this->source = $source;
         $this->target = $target;
         $this->entityType = $entityType;
+        $this->capabilities = $capabilities;
     }
 
     public function getKey(): RouteKeyInterface
@@ -53,5 +63,13 @@ class RouteGetResult implements RouteInterface
     public function getEntityType(): string
     {
         return $this->entityType;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getCapabilities(): array
+    {
+        return $this->capabilities;
     }
 }
