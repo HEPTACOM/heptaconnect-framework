0cdb4963780e4f19dd43954830848cd1139fa889
Julian Krzefski <krzefski@heptacom.de>
2021-12-18 16:11:42 +0100

    CONNECT-291: Add JobScheduleAction

---
+++ src/StorageBase/Contract/Action/Job/Schedule/JobScheduleActionInterface.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Storage\Base\Contract\Action\Job\Schedule;
+
+interface JobScheduleActionInterface
+{
+    public function schedule(JobSchedulePayload $payload): JobScheduleResult;
+}
