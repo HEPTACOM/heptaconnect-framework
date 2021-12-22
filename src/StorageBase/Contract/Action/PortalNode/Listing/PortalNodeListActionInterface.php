fbcc00c18cb5b68c673ba48db7d46cfa9580f5c1
Joshua Behrens <behrens@heptacom.de>
2021-12-22 08:48:01 +0100

    Add PortalNodeListAction
    CONNECT-126

---
+++ src/StorageBase/Contract/Action/PortalNode/Listing/PortalNodeListActionInterface.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Storage\Base\Contract\Action\PortalNode\Listing;
+
+interface PortalNodeListActionInterface
+{
+    /**
+     * @return iterable<PortalNodeListResult>
+     */
+    public function list(): iterable;
+}
