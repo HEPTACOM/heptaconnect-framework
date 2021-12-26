ff57961df784d3f41da883abab9d9087694ae0fe
Joshua Behrens <behrens@heptacom.de>
2021-12-26 03:56:01 +0100

    Add PortalNodeOverviewAction
    CONNECT-126

---
+++ src/StorageBase/Contract/Action/PortalNode/Overview/PortalNodeOverviewActionInterface.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Storage\Base\Contract\Action\PortalNode\Overview;
+
+use Heptacom\HeptaConnect\Storage\Base\Exception\InvalidOverviewCriteriaException;
+
+interface PortalNodeOverviewActionInterface
+{
+    /**
+     * @throws InvalidOverviewCriteriaException
+     *
+     * @return iterable<PortalNodeOverviewResult>
+     */
+    public function overview(PortalNodeOverviewCriteria $criteria): iterable;
+}
