eaa9c7307eb0aa7abad97780dc40a9883a40d1e7
Joshua Behrens <behrens@heptacom.de>
2021-12-26 17:10:45 +0100

    Remove PortalNodeRepositoryContract::read
    CONNECT-126

--- src/StorageBase/Contract/Repository/PortalNodeRepositoryContract.php
+++ src/StorageBase/Contract/Repository/PortalNodeRepositoryContract.php
@@ -3,17 +3,6 @@ declare(strict_types=1);
 
 namespace Heptacom\HeptaConnect\Storage\Base\Contract\Repository;
 
-use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
-use Heptacom\HeptaConnect\Storage\Base\Exception\NotFoundException;
-use Heptacom\HeptaConnect\Storage\Base\Exception\UnsupportedStorageKeyException;
-
 abstract class PortalNodeRepositoryContract
 {
-    /**
-     * @psalm-return class-string<\Heptacom\HeptaConnect\Portal\Base\Portal\Contract\PortalContract>
-     *
-     * @throws NotFoundException
-     * @throws UnsupportedStorageKeyException
-     */
-    abstract public function read(PortalNodeKeyInterface $portalNodeKey): string;
 }
