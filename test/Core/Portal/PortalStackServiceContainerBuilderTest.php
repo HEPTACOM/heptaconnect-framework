6ab68cea0fc5ff5c131aafe31466b3d154c94fcb
Joshua Behrens <behrens@heptacom.de>
2022-01-06 03:30:25 +0100

    Add PortalContainerServiceProviderInterface to provide service
    CONNECT-200

--- test/Core/Portal/PortalStackServiceContainerBuilderTest.php
+++ test/Core/Portal/PortalStackServiceContainerBuilderTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Heptacom\HeptaConnect\Core\Test\Portal;
 
 use Composer\Autoload\ClassLoader;
+use Heptacom\HeptaConnect\Core\Bridge\Portal\PortalContainerServiceProviderInterface;
 use Heptacom\HeptaConnect\Core\Configuration\Contract\ConfigurationServiceInterface;
 use Heptacom\HeptaConnect\Core\Portal\FlowComponentRegistry;
 use Heptacom\HeptaConnect\Core\Portal\PortalStackServiceContainerBuilder;
@@ -26,6 +27,7 @@ use Heptacom\HeptaConnect\Portal\Base\Serialization\Contract\NormalizationRegist
 use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
 use Heptacom\HeptaConnect\Portal\Base\Support\Contract\DeepCloneContract;
 use Heptacom\HeptaConnect\Portal\Base\Support\Contract\DeepObjectIteratorContract;
+use Heptacom\HeptaConnect\Portal\Base\Web\Http\Contract\HttpClientUtilityFactoryInterface;
 use Heptacom\HeptaConnect\Portal\Base\Web\Http\HttpHandlerUrlProviderInterface;
 use Heptacom\HeptaConnect\Storage\Base\Contract\StorageKeyGeneratorContract;
 use HeptacomFixture\Portal\A\AutomaticService\ExceptionNotInContainer;
@@ -128,6 +130,7 @@ class PortalStackServiceContainerBuilderTest extends TestCase
         static::assertTrue($container->has(RequestFactoryInterface::class));
         static::assertTrue($container->has(ResourceLockFacade::class));
         static::assertTrue($container->has(UriFactoryInterface::class));
+        static::assertTrue($container->has(HttpClientUtilityFactoryInterface::class));
 
         static::assertTrue($container->has(HttpHandlerUrlProviderInterface::class));
 
