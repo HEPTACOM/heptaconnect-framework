82274cfa74ce39f4fe80480c6564222fb83aab40
Julian Krzefski <krzefski@heptacom.de>
2022-02-18 23:43:48 +0100

    Add final to DTO classes

--- src/StorageBase/Action/Identity/Overview/IdentityOverviewResult.php
+++ src/StorageBase/Action/Identity/Overview/IdentityOverviewResult.php
@@ -10,7 +10,7 @@ use Heptacom\HeptaConnect\Dataset\Base\Support\AttachmentAwareTrait;
 use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\MappingNodeKeyInterface;
 use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
 
-class IdentityOverviewResult implements AttachmentAwareInterface
+final class IdentityOverviewResult implements AttachmentAwareInterface
 {
     use AttachmentAwareTrait;
 
