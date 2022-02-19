89c4ec1cca34617163871e4bff8d4cd279fac3c3
Julian Krzefski <krzefski@heptacom.de>
2022-02-19 18:55:18 +0100

    CONNECT-25: Add FileReferenceContract and FileReferenceCollection

---
+++ src/DatasetBase/File/FileReferenceContract.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Dataset\Base\File;
+
+use Heptacom\HeptaConnect\Dataset\Base\Contract\AttachableInterface;
+
+class FileReferenceContract implements AttachableInterface
+{
+}
