d64a9e2790bd044b7f6cba978bad7aac0377ef32
Julian Krzefski <krzefski@heptacom.de>
2022-02-19 18:57:13 +0100

    CONNECT-25: Add FileReferenceFactory and implementations for contents, publicUrl and request strategies

---
+++ src/PortalBase/File/FileReferenceFactoryContract.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Portal\Base\File;
+
+use Heptacom\HeptaConnect\Dataset\Base\File\FileReferenceContract;
+use Psr\Http\Message\RequestInterface;
+
+abstract class FileReferenceFactoryContract
+{
+    abstract public function fromPublicUrl(string $publicUrl): FileReferenceContract;
+
+    abstract public function fromRequest(RequestInterface $request): FileReferenceContract;
+
+    abstract public function fromContents(string $contents): FileReferenceContract;
+}
