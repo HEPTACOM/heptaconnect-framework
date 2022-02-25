0b7287768c5afb2b6b6e74cce35ab24a337c4a1c
Joshua Behrens <behrens@heptacom.de>
2022-02-25 21:54:33 +0100

    Do not make PostProcessorDataBag final
    CONNECT-317

--- src/PortalBase/Reception/Support/PostProcessorDataBag.php
+++ src/PortalBase/Reception/Support/PostProcessorDataBag.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Heptacom\HeptaConnect\Portal\Base\Reception\Support;
 
-final class PostProcessorDataBag
+class PostProcessorDataBag
 {
     /**
      * @var array<int, object[]>
