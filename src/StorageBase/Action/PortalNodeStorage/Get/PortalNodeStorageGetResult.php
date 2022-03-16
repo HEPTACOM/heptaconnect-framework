4fdb24b8518e6c0ad3d652609bd0bfad78b91bd3
Joshua Behrens <behrens@heptacom.de>
2022-03-16 01:49:47 +0100

    Split PortalNodeStorageGetResult and PortalNodeStorageListResult into base class
    CONNECT-126

--- src/StorageBase/Action/PortalNodeStorage/Get/PortalNodeStorageGetResult.php
+++ src/StorageBase/Action/PortalNodeStorage/Get/PortalNodeStorageGetResult.php
@@ -4,49 +4,8 @@ declare(strict_types=1);
 
 namespace Heptacom\HeptaConnect\Storage\Base\Action\PortalNodeStorage\Get;
 
-use Heptacom\HeptaConnect\Dataset\Base\AttachmentCollection;
-use Heptacom\HeptaConnect\Dataset\Base\Contract\AttachmentAwareInterface;
-use Heptacom\HeptaConnect\Dataset\Base\Support\AttachmentAwareTrait;
-use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
+use Heptacom\HeptaConnect\Storage\Base\Action\PortalNodeStorage\PortalNodeStorageItemContract;
 
-final class PortalNodeStorageGetResult implements AttachmentAwareInterface
+final class PortalNodeStorageGetResult extends PortalNodeStorageItemContract
 {
-    use AttachmentAwareTrait;
-
-    private PortalNodeKeyInterface $portalNodeKey;
-
-    private string $storageKey;
-
-    private string $type;
-
-    private string $value;
-
-    public function __construct(PortalNodeKeyInterface $portalNodeKey, string $storageKey, string $type, string $value)
-    {
-        $this->attachments = new AttachmentCollection();
-        $this->portalNodeKey = $portalNodeKey;
-        $this->storageKey = $storageKey;
-        $this->type = $type;
-        $this->value = $value;
-    }
-
-    public function getPortalNodeKey(): PortalNodeKeyInterface
-    {
-        return $this->portalNodeKey;
-    }
-
-    public function getStorageKey(): string
-    {
-        return $this->storageKey;
-    }
-
-    public function getType(): string
-    {
-        return $this->type;
-    }
-
-    public function getValue(): string
-    {
-        return $this->value;
-    }
 }
