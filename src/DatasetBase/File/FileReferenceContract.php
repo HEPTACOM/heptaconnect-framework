3b8a27e42d9626f4e6120c24bf1114bc17a46b35
Julian Krzefski <krzefski@heptacom.de>
2022-03-25 22:09:49 +0100

    CONNECT-25: Make contract class abstract

--- src/DatasetBase/File/FileReferenceContract.php
+++ src/DatasetBase/File/FileReferenceContract.php
@@ -7,7 +7,7 @@ namespace Heptacom\HeptaConnect\Dataset\Base\File;
 use Heptacom\HeptaConnect\Dataset\Base\Contract\AttachableInterface;
 use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
 
-class FileReferenceContract implements AttachableInterface
+abstract class FileReferenceContract implements AttachableInterface
 {
     private PortalNodeKeyInterface $portalNodeKey;
 
