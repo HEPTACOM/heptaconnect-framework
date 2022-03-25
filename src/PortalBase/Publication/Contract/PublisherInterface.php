89e7df62d967a1350bead076f7f859edd8c39390
Joshua Behrens <behrens@heptacom.de>
2022-03-25 20:41:12 +0100

    $ make cs-fix-php

--- src/PortalBase/Publication/Contract/PublisherInterface.php
+++ src/PortalBase/Publication/Contract/PublisherInterface.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace Heptacom\HeptaConnect\Portal\Base\Publication\Contract;
 
-use Heptacom\HeptaConnect\Dataset\Base\Contract\DatasetEntityContract;
 use Heptacom\HeptaConnect\Portal\Base\Mapping\MappingComponentCollection;
-use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
 
 interface PublisherInterface
 {
