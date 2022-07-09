5f5a9557a1b360b0d3e47e9d92cf57cbb70d6fb5
Joshua Behrens <behrens@heptacom.de>
2022-07-09 14:26:32 +0200

    Amend naming
    CONNECT-578

--- src/PortalBase/Reception/ReceiverCollection.php
+++ src/PortalBase/Reception/ReceiverCollection.php
@@ -19,7 +19,7 @@ class ReceiverCollection extends AbstractObjectCollection
     public function bySupport(EntityTypeClassString $entityType): iterable
     {
         return $this->filter(
-            static fn (ReceiverContract $emitter): bool => $entityType->same($emitter->getSupportedEntityType())
+            static fn (ReceiverContract $receiver): bool => $entityType->same($receiver->getSupportedEntityType())
         );
     }
 
