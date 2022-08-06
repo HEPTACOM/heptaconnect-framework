423a6ed1ba23653925873fe3f0e5ac1b9628074a
Joshua Behrens <behrens@heptacom.de>
2022-08-06 13:02:10 +0200

    $ make cs-fix
    CONNECT-418

--- src/UiAdminBase/Action/Route/RouteAdd/RouteAddPayload.php
+++ src/UiAdminBase/Action/Route/RouteAdd/RouteAddPayload.php
@@ -6,7 +6,6 @@ namespace Heptacom\HeptaConnect\Ui\Admin\Base\Action\Route\RouteAdd;
 
 use Heptacom\HeptaConnect\Dataset\Base\AttachmentCollection;
 use Heptacom\HeptaConnect\Dataset\Base\Contract\AttachmentAwareInterface;
-use Heptacom\HeptaConnect\Dataset\Base\Contract\DatasetEntityContract;
 use Heptacom\HeptaConnect\Dataset\Base\EntityType;
 use Heptacom\HeptaConnect\Dataset\Base\Support\AttachmentAwareTrait;
 use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
