f7cc3bade7830705e967970d4edd4b3c8c87e649
Joshua Behrens <behrens@heptacom.de>
2022-09-03 17:44:32 +0200

    Use new collection methods to have more readable code
    CONNECT-572

--- src/PortalBase/Mapping/MappingComponentCollection.php
+++ src/PortalBase/Mapping/MappingComponentCollection.php
@@ -6,6 +6,8 @@ namespace Heptacom\HeptaConnect\Portal\Base\Mapping;
 
 use Heptacom\HeptaConnect\Dataset\Base\Contract\DatasetEntityContract;
 use Heptacom\HeptaConnect\Dataset\Base\EntityType;
+use Heptacom\HeptaConnect\Dataset\Base\EntityTypeCollection;
+use Heptacom\HeptaConnect\Dataset\Base\ScalarCollection\StringCollection;
 use Heptacom\HeptaConnect\Dataset\Base\Support\AbstractObjectCollection;
 use Heptacom\HeptaConnect\Portal\Base\Mapping\Contract\MappingComponentStructContract;
 use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
@@ -38,30 +40,14 @@ class MappingComponentCollection extends AbstractObjectCollection
      */
     public function getEntityTypes(): array
     {
-        /** @var string[] $result */
-        /** @psalm-var class-string<DatasetEntityContract>[] $result */
-        $result = [];
+        $entityTypes = (new EntityTypeCollection($this->column('getEntityType')))->unique()->asArray();
 
-        /** @var MappingComponentStructContract $mappingComponent */
-        foreach ($this->getIterator() as $mappingComponent) {
-            if (!\in_array((string) $mappingComponent->getEntityType(), $result, true)) {
-                $result[] = (string) $mappingComponent->getEntityType();
-            }
-        }
-
-        return $result;
+        return \array_map(static fn (EntityType $type): string => (string) $type, $entityTypes);
     }
 
     public function getPortalNodeKeys(): PortalNodeKeyCollection
     {
-        $preResult = [];
-
-        foreach ($this->getIterator() as $mappingComponent) {
-            $portalNodeKey = $mappingComponent->getPortalNodeKey();
-            $preResult[\json_encode($portalNodeKey)] = $portalNodeKey;
-        }
-
-        return new PortalNodeKeyCollection(\array_values($preResult));
+        return (new PortalNodeKeyCollection($this->column('getPortalNodeKey')))->unique();
     }
 
     /**
@@ -69,13 +55,7 @@ class MappingComponentCollection extends AbstractObjectCollection
      */
     public function getExternalIds(): array
     {
-        $preResult = [];
-
-        foreach ($this->getIterator() as $mappingComponent) {
-            $preResult[$mappingComponent->getExternalId()] = $mappingComponent->getExternalId();
-        }
-
-        return \array_values($preResult);
+        return (new StringCollection($this->column('getExternalId')))->unique()->asArray();
     }
 
     /**
