061d454f51ed964b513db0dce9e28a63f16ec4cd
Joshua Behrens <behrens@heptacom.de>
2022-10-08 11:07:47 +0200

    Amend documentation
    CONNECT-589

--- src/UiAdminBase/Contract/Audit/AuditableDataAwareInterface.php
+++ src/UiAdminBase/Contract/Audit/AuditableDataAwareInterface.php
@@ -7,7 +7,7 @@ namespace Heptacom\HeptaConnect\Ui\Admin\Base\Contract\Audit;
 interface AuditableDataAwareInterface
 {
     /**
-     * Returns auditable data, that is JSON serializable.
+     * Returns auditable data, that must be JSON serializable.
      */
     public function getAuditableData(): array;
 }
