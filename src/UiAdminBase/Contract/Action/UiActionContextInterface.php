4d6aab83af2ac6156aec0db51cc9480db0a111e3
Joshua Behrens <behrens@heptacom.de>
2022-10-08 11:09:12 +0200

    Move attachment aware
    CONNECT-589

--- src/UiAdminBase/Contract/Action/UiActionContextInterface.php
+++ src/UiAdminBase/Contract/Action/UiActionContextInterface.php
@@ -4,13 +4,12 @@ declare(strict_types=1);
 
 namespace Heptacom\HeptaConnect\Ui\Admin\Base\Contract\Action;
 
-use Heptacom\HeptaConnect\Dataset\Base\Contract\AttachmentAwareInterface;
 use Heptacom\HeptaConnect\Ui\Admin\Base\Audit\UiAuditContext;
 
 /**
  * Describes an admin UI action specific context.
  */
-interface UiActionContextInterface extends AttachmentAwareInterface
+interface UiActionContextInterface
 {
     /**
      * Gets the user information related to the admin UI action call.
