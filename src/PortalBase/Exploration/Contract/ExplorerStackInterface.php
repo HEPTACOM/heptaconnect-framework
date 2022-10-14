d770dc77bede03e6e352edf743a6517daad3dc31
Joshua Behrens <behrens@heptacom.de>
2022-10-14 19:37:28 +0200

    Add documentation
    CONNECT-90

--- src/PortalBase/Exploration/Contract/ExplorerStackInterface.php
+++ src/PortalBase/Exploration/Contract/ExplorerStackInterface.php
@@ -7,6 +7,10 @@ namespace Heptacom\HeptaConnect\Portal\Base\Exploration\Contract;
 use Heptacom\HeptaConnect\Dataset\Base\Contract\DatasetEntityContract;
 use Heptacom\HeptaConnect\Dataset\Base\EntityType;
 
+/**
+ * Describes an ordered stack of explorers, that are grouped by their supported @see EntityType
+ * As a stack the @see next execution reduces the list by one explorer.
+ */
 interface ExplorerStackInterface
 {
     /**
