3eaae5722bcb3fc4bc9da5fc44e14e07ede5af56
Joshua Behrens <behrens@heptacom.de>
2022-10-30 01:02:41 +0200

    Extract path URI conversion into own class
    CONNECT-602

--- test/Core/Portal/File/Filesystem/FilesystemTest.php
+++ test/Core/Portal/File/Filesystem/FilesystemTest.php
@@ -4,22 +4,25 @@ declare(strict_types=1);
 
 namespace Heptacom\HeptaConnect\Core\Test\Portal\File\Filesystem;
 
+use Heptacom\HeptaConnect\Core\File\Filesystem\StreamUriSchemePathConverter;
 use Heptacom\HeptaConnect\Core\Portal\File\Filesystem\Filesystem;
 use Http\Discovery\Psr17FactoryDiscovery;
 use PHPUnit\Framework\TestCase;
 
 /**
- * @covers \Heptacom\HeptaConnect\Core\Portal\Filesystem\Filesystem
+ * @covers \Heptacom\HeptaConnect\Core\File\Filesystem\StreamUriSchemePathConverter
+ * @covers \Heptacom\HeptaConnect\Core\Portal\File\Filesystem\Filesystem
+ * @covers \Heptacom\HeptaConnect\Portal\Base\File\Filesystem\Exception\UnexpectedFormatOfUriException
  */
 class FilesystemTest extends TestCase
 {
     public function testToStoragePathWorksWithPathOnlyUri(): void
     {
         $filesystem = $this->createFilesystem();
-        static::assertEquals('heptaconnect-portal-node+123:this-is-a-file.csv', $filesystem->toStoragePath('this-is-a-file.csv'));
-        static::assertEquals('heptaconnect-portal-node+123:nested/path/this-is-a-file.csv', $filesystem->toStoragePath('nested/path/this-is-a-file.csv'));
-        static::assertEquals('heptaconnect-portal-node+123:foobar.d/this-is-a-file.csv', $filesystem->toStoragePath('foobar.d/this-is-a-file.csv'));
-        static::assertEquals('heptaconnect-portal-node+123:', $filesystem->toStoragePath(''));
+        static::assertEquals('heptaconnect-portal-node+123://this-is-a-file.csv', $filesystem->toStoragePath('this-is-a-file.csv'));
+        static::assertEquals('heptaconnect-portal-node+123://nested/path/this-is-a-file.csv', $filesystem->toStoragePath('nested/path/this-is-a-file.csv'));
+        static::assertEquals('heptaconnect-portal-node+123://foobar.d/this-is-a-file.csv', $filesystem->toStoragePath('foobar.d/this-is-a-file.csv'));
+        static::assertEquals('heptaconnect-portal-node+123://', $filesystem->toStoragePath(''));
     }
 
     public function testToStoragePathFailsWithSchemaInUri(): void
@@ -53,8 +56,8 @@ class FilesystemTest extends TestCase
     public function testFromStoragePathWorksWithRightSchemeInUri(): void
     {
         $filesystem = $this->createFilesystem();
-        static::assertSame('this-is-a-url.csv', $filesystem->fromStoragePath('heptaconnect-portal-node+123:this-is-a-url.csv'));
-        static::assertSame('dir.d/tested/this-is-a-url.csv', $filesystem->fromStoragePath('heptaconnect-portal-node+123:dir.d/tested/this-is-a-url.csv'));
+        static::assertSame('this-is-a-url.csv', $filesystem->fromStoragePath('heptaconnect-portal-node+123://this-is-a-url.csv'));
+        static::assertSame('dir.d/tested/this-is-a-url.csv', $filesystem->fromStoragePath('heptaconnect-portal-node+123://dir.d/tested/this-is-a-url.csv'));
     }
 
     public function testFromStoragePathFailsWithWrongSchemeInUri(): void
@@ -80,20 +83,20 @@ class FilesystemTest extends TestCase
 
     public function testFromStoragePathFailsWithQueryInUri(): void
     {
-        static::expectExceptionCode(1666942814);
+        static::expectExceptionCode(1666942813);
 
-        $this->createFilesystem()->fromStoragePath('heptaconnect-portal-node+123:this-is-a-url.csv?foo=bar');
+        $this->createFilesystem()->fromStoragePath('heptaconnect-portal-node+123://this-is-a-url.csv?foo=bar');
     }
 
     public function testFromStoragePathFailsWithFragmentInUri(): void
     {
-        static::expectExceptionCode(1666942815);
+        static::expectExceptionCode(1666942814);
 
-        $this->createFilesystem()->fromStoragePath('heptaconnect-portal-node+123:this-is-a-url.csv#form');
+        $this->createFilesystem()->fromStoragePath('heptaconnect-portal-node+123://this-is-a-url.csv#form');
     }
 
     private function createFilesystem(): Filesystem
     {
-        return new Filesystem(Psr17FactoryDiscovery::findUriFactory(), 'heptaconnect-portal-node+123');
+        return new Filesystem(new StreamUriSchemePathConverter(Psr17FactoryDiscovery::findUriFactory(), 'heptaconnect-portal-node+123'));
     }
 }
