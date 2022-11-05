414a6b01fde244b402b4207a6c8cb2900be9f607
Joshua Behrens <behrens@heptacom.de>
2022-11-05 16:11:43 +0100

    Add test portal for test suite portal package
    CONNECT-602

---
+++ test-suite-portal-test-portal/src/TestPortal.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HeptacomFixture\TestSuitePortal\TestPortal;
+
+use Heptacom\HeptaConnect\Portal\Base\Portal\Contract\PortalContract;
+
+final class TestPortal extends PortalContract
+{
+}
