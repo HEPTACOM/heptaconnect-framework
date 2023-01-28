4f7d5a403993037831eff37224b9a7754882c787
Julian Krzefski <krzefski@heptacom.de>
2023-01-28 16:15:32 +0100

    Move registration of container configuration files from PortalStackServiceContainerBuilder into PackageContract

--- src/PortalBase/Portal/Contract/PackageContract.php
+++ src/PortalBase/Portal/Contract/PackageContract.php
@@ -4,20 +4,34 @@ declare(strict_types=1);
 
 namespace Heptacom\HeptaConnect\Portal\Base\Portal\Contract;
 
+use Heptacom\HeptaConnect\Core\Portal\Exception\DelegatingLoaderLoadException;
 use Heptacom\HeptaConnect\Dataset\Base\Contract\AttachableInterface;
 use Heptacom\HeptaConnect\Dataset\Base\Contract\CollectionInterface;
 use Heptacom\HeptaConnect\Dataset\Base\Contract\DatasetEntityContract;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Loader\DelegatingLoader;
+use Symfony\Component\Config\Loader\LoaderResolver;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 /**
- * This contract must only be extended by @see PortalContract and @see PortalExtensionContract
- * Its only purpose is to combine their features in a single class.
- *
- * @internal
+ * This is the contract for all packages in a portal container, and it must be extended by:
+ *   - any portal @see PortalContract
+ *   - any portal extension @see PortalExtensionContract
+ *   - any additional packages returned by @see PackageContract::getAdditionalPackages()
  */
 abstract class PackageContract
 {
     use PathMethodsTrait;
+
+    /**
+     * @final This method will become final in version 0.10
+     */
+    public function __construct()
+    {
+    }
 
     /**
      * Returns all FQCNs that must not to be present in a service's class hierarchy.
@@ -38,5 +52,41 @@ abstract class PackageContract
 
     public function buildContainer(ContainerBuilder $containerBuilder): void
     {
+        $this->registerContainerFile($containerBuilder);
+    }
+
+    /**
+     * @throws DelegatingLoaderLoadException
+     */
+    final protected function registerContainerFile(ContainerBuilder $containerBuilder): void
+    {
+        $containerConfigurationPath = $this->getContainerConfigurationPath();
+
+        $fileLocator = new FileLocator($containerConfigurationPath);
+        $loaderResolver = new LoaderResolver([
+            new XmlFileLoader($containerBuilder, $fileLocator),
+            new YamlFileLoader($containerBuilder, $fileLocator),
+            new PhpFileLoader($containerBuilder, $fileLocator),
+        ]);
+        $delegatingLoader = new DelegatingLoader($loaderResolver);
+        $directory = $containerConfigurationPath . \DIRECTORY_SEPARATOR . 'services.';
+        $files = [
+            $directory . 'yml',
+            $directory . 'yaml',
+            $directory . 'xml',
+            $directory . 'php',
+        ];
+
+        foreach ($files as $serviceDefinitionPath) {
+            if (!\is_file($serviceDefinitionPath)) {
+                continue;
+            }
+
+            try {
+                $delegatingLoader->load($serviceDefinitionPath);
+            } catch (\Throwable $throwable) {
+                throw new DelegatingLoaderLoadException($serviceDefinitionPath, $throwable);
+            }
+        }
     }
 }
