23991cbb033412a828e28fb539c347f7f445fe46
Joshua Behrens <behrens@heptacom.de>
2023-03-05 15:42:11 +0100

    Remove phpstan ignore

--- src/StorageBase/IdentityRedirectKeyCollection.php
+++ src/StorageBase/IdentityRedirectKeyCollection.php
@@ -14,7 +14,6 @@ class IdentityRedirectKeyCollection extends AbstractCollection
 {
     protected function isValidItem($item): bool
     {
-        /* @phpstan-ignore-next-line treatPhpDocTypesAsCertain checks soft check but this is the hard check */
         return $item instanceof IdentityRedirectKeyInterface;
     }
 }
