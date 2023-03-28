e640df5d7d09b4ca39a4ff48ffed34b283cee3c2
Julian Krzefski <krzefski@heptacom.de>
2023-03-28 22:29:12 +0200

    Add method "forward" to HttpHandleContextInterface

--- src/PortalBase/Web/Http/Contract/HttpHandleContextInterface.php
+++ src/PortalBase/Web/Http/Contract/HttpHandleContextInterface.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace Heptacom\HeptaConnect\Portal\Base\Web\Http\Contract;
 
 use Heptacom\HeptaConnect\Portal\Base\Portal\Contract\PortalNodeContextInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
+use Psr\Http\Message\UriInterface;
 
 /**
  * Describes HTTP handler specific contexts.
@@ -14,4 +17,18 @@ use Heptacom\HeptaConnect\Portal\Base\Portal\Contract\PortalNodeContextInterface
  */
 interface HttpHandleContextInterface extends PortalNodeContextInterface
 {
+    /**
+     * @param UriInterface|string $uri
+     * @param string $method
+     * @param StreamInterface|array|string|null $body
+     * @param array<string, string>|array<string, array<array-key, string>> $headers
+     *
+     * @return ResponseInterface
+     */
+    public function forward(
+        $uri,
+        string $method = 'GET',
+        $body = null,
+        array $headers = []
+    ): ResponseInterface;
 }
